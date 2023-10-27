87218da3e19c138af8075b1e4ed41433c4bde8d5
Davydova Olha <o.chookity@gmail.com>
2023-10-27 11:23:10 +0300

    Add Tag list page URL to urlpatterns

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from todo.views import TaskListView, IndexView
+from todo.views import (
+    TaskListView,
+    IndexView,
+    TagListView
+)
+
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
