69ad8527b52d3e8a596eb43219673e498e2e5667
Davydova Olha <o.chookity@gmail.com>
2023-10-27 11:39:17 +0300

    Create `TaskCreateView`

--- todo/views.py
+++ todo/views.py
@@ -14,5 +14,11 @@ class TaskListView(generic.ListView):
     model = Task
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
