942e731250683a81746a7b16bf0fe8b4d50c7f6b
Davydova Olha <o.chookity@gmail.com>
2023-10-27 12:21:17 +0300

    Add Task Delete page URL to urlpatterns

--- todo/urls.py
+++ todo/urls.py
@@ -8,7 +8,8 @@ from todo.views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
-    TaskUpdateView
+    TaskUpdateView,
+    TaskDeleteView
 )
 
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
