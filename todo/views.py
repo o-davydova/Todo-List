6743bedc386d7b32a4af312011a01f4d6078a67d
Davydova Olha <o.chookity@gmail.com>
2023-10-27 12:10:32 +0300

    Create `TaskUpdateView`

--- todo/views.py
+++ todo/views.py
@@ -20,6 +20,12 @@ class TaskCreateView(generic.CreateView):
     success_url = reverse_lazy("todo:task-list")
 
 
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = ["content", "deadline", "is_done", "tags"]
+    success_url = reverse_lazy("todo:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
 
