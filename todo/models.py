00bf8b5e633b43b2089a07a44082a2c284b0de9a
Davydova Olha <o.chookity@gmail.com>
2023-10-27 10:24:24 +0300

    Add __str__ methods to models

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField()
@@ -11,3 +14,6 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return self.content
